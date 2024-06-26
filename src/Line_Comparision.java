e447099920c75b807b66ca54556dc27b6b94c995
Kavya <kavyagopal94@gmail.com>
2024-06-26 11:58:49 +0530

    Begining_Line_computation

---
+++ src/Line_Comparision.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Line_Comparision
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+}
